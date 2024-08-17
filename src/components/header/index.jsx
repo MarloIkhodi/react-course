cddbbd42878c072204d17df6d756af5c5280756a
MarloIkhodi <deivid.serafim03@gmail.com>
2024-08-17 19:17:46 -0300

    added a lot

---
+++ src/components/header/index.jsx
@@ -0,0 +1,21 @@
+import "./styles.css";
+import Logo from "../../assets/images/logo.svg";
+import Downbox from "../../assets/images/chevron-down.svg";
+
+export default function Header() {
+  return (
+    <header>
+      <img src={Logo} alt="Site Logo" />
+      <nav>
+        <a href="">Link One</a>
+        <a href="">Link Two</a>
+        <a href="">Link Three</a>
+        <a href="">
+          Link Four
+          <img src={Downbox} alt="Down Icon" />
+        </a>
+      </nav>
+      <button>Try it for free</button>
+    </header>
+  );
+}
