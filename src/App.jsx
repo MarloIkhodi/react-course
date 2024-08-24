5957363f789dfb0663d184a5e043299223e452a4
MarloIkhodi <deivid.serafim03@gmail.com>
2024-08-24 14:21:42 -0300

    testes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
-import Home from "./pages/home";
+function Filho({ children }) {
+  return <div className="filho">{children}</div>;
+}
 
-export default function Page() {
-  return <Home />;
+export default function App() {
+  return (
+    //no caso de children, nao necessita chamar nenhum atributo, somente colocar dentro da tag
+    <Filho>
+      <p>Sou uma tag P passada por children</p>
+    </Filho>
+  );
 }
---
+++ src/components/card/index.jsx
@@ -0,0 +1,11 @@
+import "./style.css";
+export default function Card({ title, description, image, imageAlt }) {
+  //toda função Js dentro de um arquivo jsx vira um parametro
+  return (
+    <div className="card">
+      <img src={image} alt={imageAlt} />
+      <p className="first">{title}</p>
+      <p>{description}</p>
+    </div>
+  );
+}
