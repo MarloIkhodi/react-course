e6197273dec853896f3ef35c4d1b7c1716d04d35
MarloIkhodi <deivid.serafim03@gmail.com>
2024-09-14 16:15:24 -0300

    pokemoAPI

--- src/assets/class/hooks/use-hooks/pokemon-api/index.jsx
+++ src/assets/class/hooks/use-hooks/pokemon-api/index.jsx
@@ -1,22 +1,35 @@
-import { useState } from "react"
-import { useEffect } from "react"
-
+import { useState, useEffect } from "react";
 
 export default function PokemonApi() {
     const [pokemons, setPokemons] = useState([])
     const [loading, setLoading] = useState(true)
     const [error, setError] = useState(null)
-
-
-
     useEffect(() => {
         fetch("https://pokeapi.co/api/v2/pokemon/?limit=10")
             .then((res) => res.json())
-            .then((data) => console.log(data))
-            .catch((error) => { console.log(error) })
+            .then((data) => {
+                setPokemons(data.results)
+                setLoading(false)
+            })
+            .catch((error) => {
+                setError(error.message)
+                setLoading(false)
+            })
     }, [])
-
+    // if (loading) {return <p>Carregando...</p>} mesmo que {loading && (<p>Carregando...</p>)}
+    // if (error) { return <p>Errro: {error}</p>} mesmo que {error && (<p>Erro: {error}</p>)}
     return (
-        <h1>Hello World!</h1>
-    )
+        <>
+            {loading && <p>Carregando...</p>}
+            {error && <p>Erro: {error}</p>}
+            <div>
+                <h1>Lista de pokemons</h1>
+                <ul>
+                    {pokemons.map((pokemon, index) => {
+                        <li key={index}>(pokemon.name)</li>
+                    })}
+                </ul>
+            </div>
+        </>
+    );
 }
