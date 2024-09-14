8d9615175725bab13347a223178d772661878d26
MarloIkhodi <deivid.serafim03@gmail.com>
2024-09-14 10:46:33 -0300

    Accordion

--- src/assets/class/hooks/useState/accordion/index.jsx
+++ src/assets/class/hooks/useState/accordion/index.jsx
@@ -1,11 +1,18 @@
 import { useState } from "react";
 
 export default function Accordion() {
-    const [dropdown, setDropdown] = useState(false)
+    const [isOpen, setIsOpen] = useState(false)
 
     return (
-        <div>
-            <button></button>
+        <div className="accordinContainer">
+
+            <div className="accordionTitle">
+                <p>Novidades</p>
+            </div>
+            {isOpen && (<div className="accordionContent">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam veritatis veniam asperiores vero quia ut delectus inventore nesciunt ad commodi. Temporibus vel totam ipsam consequatur inventore, in molestias ea odio!</p>
+            </div>)}
+
         </div>
     )
 }
