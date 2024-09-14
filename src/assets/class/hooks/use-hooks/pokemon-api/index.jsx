258e2f309ac3893014b89d2f01915e8b952ef95e
MarloIkhodi <deivid.serafim03@gmail.com>
2024-09-14 15:23:13 -0300

    pokemoAPI

--- src/assets/class/hooks/use-hooks/pokemon-api/index.jsx
+++ src/assets/class/hooks/use-hooks/pokemon-api/index.jsx
@@ -1,4 +1,21 @@
+import { useState } from "react"
+import { useEffect } from "react"
+
+
 export default function PokemonApi() {
+    const [pokemons, setPokemons] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(null)
+
+
+
+    useEffect(() => {
+        fetch("https://pokeapi.co/api/v2/pokemon/?limit=10")
+            .then((res) => res.json())
+            .then((data) => console.log(data))
+            .catch((error) => { console.log(error) })
+    }, [])
+
     return (
         <h1>Hello World!</h1>
     )
