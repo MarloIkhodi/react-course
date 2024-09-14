0e2af304e867733001dcb0909114a7dfd57b55be
MarloIkhodi <deivid.serafim03@gmail.com>
2024-09-14 16:42:26 -0300

    Consumo de API

--- src/assets/class/hooks/use-hooks/pokemon-api/index.jsx
+++ src/assets/class/hooks/use-hooks/pokemon-api/index.jsx
@@ -16,19 +16,19 @@ export default function PokemonApi() {
                 setLoading(false)
             })
     }, [])
-    // if (loading) {return <p>Carregando...</p>} mesmo que {loading && (<p>Carregando...</p>)}
-    // if (error) { return <p>Errro: {error}</p>} mesmo que {error && (<p>Erro: {error}</p>)}
+    // if (loading) {return <p>Carregando...</p>} same as {loading && (<p>Carregando...</p>)}
+    // if (error) { return <p>Errro: {error}</p>} same as {error && (<p>Erro: {error}</p>)}
     return (
         <>
             {loading && <p>Carregando...</p>}
             {error && <p>Erro: {error}</p>}
             <div>
                 <h1>Lista de pokemons</h1>
-                <ul>
-                    {pokemons.map((pokemon, index) => {
-                        <li key={index}>(pokemon.name)</li>
-                    })}
-                </ul>
+                {pokemons.map((pokemon, i) => (
+                    <div key={i}>
+                        <a href={pokemon.url}>{pokemon.name}</a>
+                    </div>
+                ))}
             </div>
         </>
     );
